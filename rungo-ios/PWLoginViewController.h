c2895c3461e55d448cd84fb49d73d41ffacbecf6
Phil Wittig <pdwittig@gmail.com>
2014-08-25 22:34:50 -0700

    add login view controller

---
+++ rungo-ios/PWLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  PWLoginViewController.h
+//  rungo-ios
+//
+//  Created by Phillip Wittig on 8/25/14.
+//  Copyright (c) 2014 PhilWittig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PWLoginViewController : UIViewController
+
+@end
