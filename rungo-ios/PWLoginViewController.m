69b9806e5b0a78b38e1478e030f4e8a2aff32edf
Phil Wittig <pdwittig@gmail.com>
2014-08-25 23:38:44 -0700

    hide back nag button

--- rungo-ios/PWLoginViewController.m
+++ rungo-ios/PWLoginViewController.m
@@ -14,25 +14,15 @@
 
 @implementation PWLoginViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void) viewWillAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self.navigationItem setHidesBackButton:YES];
 }
 
 /*
